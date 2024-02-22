97f890828f9715b2bdcf343fd7409ee9c3517f59
Quentin Verdier <quent1.verdier@gmail.com>
2024-02-22 09:52:04 +0100

    ✨Add Product entity
    Added new fields to the Product entity class for id, code, label, type, and price.

--- src/main/java/fr/epsi/b32324c2/tpspringboot/bo/Product.java
+++ src/main/java/fr/epsi/b32324c2/tpspringboot/bo/Product.java
@@ -1,2 +1,25 @@
-package fr.epsi.b32324c2.tpspringboot.bo;public class Product {
+package fr.epsi.b32324c2.tpspringboot.bo;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "label")
+    private String label;
+
+    @Column(name = "type")
+    private ProdType type;
+
+    @Column(name = "price")
+    private Double price;
 }
